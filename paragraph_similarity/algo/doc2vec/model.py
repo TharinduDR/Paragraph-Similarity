49290c1d2ebda4ec0f272a3b0b9f704fd69ff187
TharinduDR <rhtdranasinghe@gmail.com>
2020-11-25 17:05:08 +0000

    002: Adding pipeline

--- paragraph_similarity/algo/doc2vec/model.py
+++
@@ -1,9 +0,0 @@
-import gensim
-
-
-def training(train_corpus):
-    model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=2, epochs=40)
-    model.build_vocab(train_corpus)
-    model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs)
-
-    return model
